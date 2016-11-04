37275f346e047c0a6980954f8090bad73f7272c6
Tom Emmerson <thomasemmerson@sky.com>
2016-11-04 13:08:22 +0000

    Update

--- AnagramCodeKata/StringExtensions.cs
+++ AnagramCodeKata/StringExtensions.cs
@@ -4,6 +4,9 @@ namespace AnagramCodeKata
 {
     public static class StringExtensions
     {
+        /*
+         * This is the method that needs updating during the code kata...
+         */
         public static bool IsAnagramOf(this string firstWord, string secondWord)
         {
             return string.Join("", firstWord.OrderBy(c => c)) == string.Join("", secondWord.OrderBy(c => c));
