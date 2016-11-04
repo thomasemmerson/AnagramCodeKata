e741a8a7a980d2ccb08098c52e78500d65c0316e
Tom Emmerson <thomasemmerson@sky.com>
2016-11-04 10:42:29 +0000

    First commit

---
+++ AnagramCodeKataTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramCodeKataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
